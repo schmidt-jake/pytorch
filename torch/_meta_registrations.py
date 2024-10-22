204eac885c728f65b98dba39fa1d1e7dd6c2dd0a
Laith Sakka <lsakka@meta.com>
2024-10-22 11:29:36 -0700

    Pass export from self.export to insert_deferred_runtime_asserts (#138500)
    Summary: Pull Request resolved: https://github.com/pytorch/pytorch/pull/138500
    
    Differential Revision: D64708624

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -718,6 +718,10 @@ def functional_sym_constrain_range(size, min=None, max=None, dep_token=None):
 def sym_constrain_range_for_size(size, min=None, max=None):
     # Avoid importing sympy at a module level
     from torch.fx.experimental.symbolic_shapes import _constrain_range_for_size
+    
+    if min is None and max is None:
+        torch._check_is_size(size)
+        return
 
     if isinstance(size, (SymFloat, SymBool)):
         raise ValueError("Constraining SymFloat or Symbool is nyi")
